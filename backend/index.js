477e3aa18056ea9597e5cf2b6c7e17878706dd05
kanhadewangan <workkanhadewangan@gmail.com>
2025-12-12 22:22:13 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: ['http://127.0.0.1:5500', 'http://localhost:5500'],
+    origin: ['https://syncarena.onrender.com/'],
     credentials: true
   },
   transports: ["polling", "websocket"],
